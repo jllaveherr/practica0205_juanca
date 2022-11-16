670a3fb6402947f1dec0d07f01c6f892eec2530e
jllaveherr <jllaveherr@educacion.navarra.es>
2022-11-16 20:21:23 +0100

    hecho hasta el 3

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+productos = {"Pan":1.40, "Huevos":2.30, "Cebolla":0.85, "Aceite":4.33}
+producto = input("¿Qué producto quieres? ")
+if producto not in productos:
+    print("Lo siento este producto no tenemos")
+else:
+    numero_unidades = float(input("¿Cuántas unidades quieres? "))
+    print(numero_unidades, "unidades de", producto, "valen", productos[producto] * numero_unidades, "€")
